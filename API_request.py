1e769b39c2643b052fba01b884e9eece7c49a443
Pablo Pinillos <pinillosdev@gmail.com>
2025-03-15 13:40:40 -0500

    api_get_request function improved. Also, better exceptions handling with finishProcedure new function.

--- API_request.py
+++ API_request.py
@@ -1,32 +1,42 @@
-import requests, json, creds
+import requests, json, creds, sys
 
-def api_get_request(url: str, token: str) -> dict[str, any]:
+def finishProcedure():
+    message = "\n\nProgram has finished.\n@0xPinillos"
+    print(message)
+    return sys.exit()
+
+
+def api_get_request(url: str, token: str):
     headers = {
         "Authorization": f"Bearer {token}",
     }
 
     try:
-        response = requests.get(url, headers=headers, timeout=38)
+        response = requests.get(url, headers=headers, timeout=38) # HTTP GET request to reach data
         response.raise_for_status() # Raise HTTPError for bad responses (4xx or 5xx)
         contentResponse = response.content
         return contentResponse
 
+    # Exceptions to handle different error may occur during execution.
+    # Return None to handle through all code flow
     except requests.exceptions.RequestException as e:
         print(f"\nError fetching data from {url}: {e}")
-        return {}  # Return empty dict on error, or you may choose to raise the exception.
+        return None
     except json.JSONDecodeError as e:
         print(f"\nError decoding JSON from {url}: {e}")
-        return {}
+        return None
     except ValueError as e:
         print(f"\nValue Error: {e}")
-        return {}
+        return None
     except Exception as e:
         print(f"\nAn unexpected error occurred: {e}")
-        return {}
+        return None
 
 
 def main():
     data = api_get_request(creds.urlApi, creds.authenticationToken)
+    if data != None: pass
+    else: finishProcedure()
     print(data)
 
 
